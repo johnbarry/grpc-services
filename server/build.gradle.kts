d6f0b38c6ec310aafeef675cef96698e0c649d30
johnbarry <john.barry@me.com>
2021-12-26 23:59:12 -0500

    gradle files too

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
-
 }
 
 group = "org.jpb"
@@ -27,15 +26,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	//api(kotlin("stdlib"))
 	api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.ext["kotlinVersion"]}")
 	api("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${rootProject.ext["kotlinVersion"]}")
-
-	api("io.grpc:grpc-protobuf:${rootProject.ext["grpcVersion"]}")
-	api("com.google.protobuf:protobuf-java-util:${rootProject.ext["protobufVersion"]}")
-	api("com.google.protobuf:protobuf-kotlin:${rootProject.ext["protobufVersion"]}")
-	api("io.grpc:grpc-kotlin-stub:${rootProject.ext["grpcKotlinVersion"]}")
-
 }
 java {
 	sourceCompatibility = JavaVersion.VERSION_11
