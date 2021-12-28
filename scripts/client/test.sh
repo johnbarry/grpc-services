ee54682e3fd7cd972f223f20ac6800faa4a99af9
johnbarry <john.barry@me.com>
2021-12-28 13:28:21 -0500

    Switch service from load balancer to ClusterIP and change port nane to gRPC to support gRPC metrics from Istio's kiali dashboard

--- scripts/client/test.sh
+++ scripts/client/test.sh
@@ -1,5 +1,5 @@
 GITDIR=$HOME/git
-GRPC_HOST=$(kubectl get services grpc-demo-grpc | awk '{print $4}' | tail -n +2)
+GRPC_HOST=$(kubectl get services grpc-demo-grpc | awk '{print $3}' | tail -n +2)
 cd "${GITDIR}"/grpc-services/proto/src/main/proto/org/jpb/grpcservice || exit
 PROTOFILE=jpbservice.proto
 echo "-----------------"
@@ -9,13 +9,13 @@ echo "-----------------"
 echo "f1..."
 for i in {1..1000}
 do
-"${GITDIR}"/grpc/cmake/build/grpc_cli call "${GRPC_HOST}":50051 f1 "number: $i" --protofiles=$PROTOFILE
+	"${GITDIR}"/grpc/cmake/build/grpc_cli call "${GRPC_HOST}":50051 f1 "number: $i" --protofiles=$PROTOFILE
 done
 echo "-----------------"
 echo "f2..."
 for i in {1..1000}
 do
-"${GITDIR}"/grpc/cmake/build/grpc_cli call "${GRPC_HOST}":50051 f2 "number: $i" --protofiles=$PROTOFILE
+	"${GITDIR}"/grpc/cmake/build/grpc_cli call "${GRPC_HOST}":50051 f2 "number: $i" --protofiles=$PROTOFILE
 done
 cd "${GITDIR}"/grpc-services/scripts/client || exit
 
