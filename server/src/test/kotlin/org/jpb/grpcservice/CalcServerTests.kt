177657c573c4a9569ae882894d66c7a71587525d
johnbarry <john.barry@me.com>
2021-12-29 11:16:49 -0500

    Resolved sourceset issue by adding to proto module.  Also add lineage to gRPC and added tests for it

--- server/src/test/kotlin/org/jpb/grpcservice/CalcServerTests.kt
+++ server/src/test/kotlin/org/jpb/grpcservice/CalcServerTests.kt
@@ -4,6 +4,7 @@ import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.runBlocking
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import org.jpb.grpcservice.proto.ANumber
 import org.jpb.grpcservice.proto.CalcGrpcKt
 import org.jpb.grpcservice.proto.aNumber
 import org.junit.jupiter.api.Test
@@ -26,6 +27,7 @@ class CalcServerTests {
 		val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build()
 		val stub = CalcGrpcKt.CalcCoroutineStub(channel)
 		runBlocking {
+			// f1(x) = x + 1
 			val response = stub.f1 (aNumber {
 				number = 20
 			})
@@ -34,12 +36,14 @@ class CalcServerTests {
 			response.lineage.getLineage(0).service shouldBe "grpcservice.Calc"
 			response.lineage.getLineage(0).procedure shouldBe "f1"
 
+			// f2(x) = x + 1000
 			val response2 = stub.f2 (response)
 			response2.number shouldBe 1_021
 			response2.lineage.lineageCount shouldBe 2
 			response2.lineage.getLineage(1).service shouldBe "grpcservice.Calc"
 			response2.lineage.getLineage(1).procedure shouldBe "f2"
 
+			// f3(x) = x + 1000000
 			val response3 = stub.f3 (response2)
 			response3.number shouldBe 1_001_021
 			response3.lineage.lineageCount shouldBe 3
